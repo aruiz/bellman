1c28e20768e95a904a6d9bee45af56114e43f1a3
Alberto Ruiz <aruiz@gnome.org>
2014-01-21 22:42:10 +0100

    Initial attempt to abstract the data storage interface

--- main.go
+++ main.go
@@ -7,19 +7,47 @@ import (
   "time"
   "runtime"
   "github.com/garyburd/redigo/redis"
-  "fmt"
 )
 
+type DataStore interface {
+  GetStateForSession (session string) (string, error)
+}
+
+type RedisDataStore struct {
+  conn        *redis.Conn
+}
+
+func (rds RedisDataStore) Connect (url string) error {
+  c, err := redis.Dial("tcp", url)
+  if err != nil {
+    return err
+  }
+
+  rds.conn = &c
+  return nil
+}
+
+func (rds RedisDataStore) GetStateForSession (session string) (string, error) {
+  if rds.conn == nil {
+    return "", nil
+  }
+
+  rep, err := (*rds.conn).Do ("GET", "sessions:" + session + ":state")
+  payload , err := redis.String(rep, err)
+  return payload, err
+}
 
 type Session struct {
   payload string
   lock    sync.RWMutex
 }
 
+
+//This handler implements the web service
 type MainHandler struct {
   /* Make a map of payloads and locks for each session being served */
   sessions     map[string]*Session
-  rconn        *redis.Conn
+  store        DataStore
 }
 
 func (mh MainHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
@@ -27,18 +55,20 @@ func (mh MainHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
   switch {
     case strings.HasPrefix(path, "/session"):
         mh.HandleSessionRequest (w, r)
+    default:
+      w.WriteHeader(http.StatusNotFound)
   }
 }
 
 func (mh MainHandler) HandleSessionRequest (w http.ResponseWriter, r *http.Request) {
   path := strings.Split(r.URL.Path, "/")
   if len (path) != 3 {
+
     return
   }
   sessionName := path[2]
   if r.Method == "GET" {
     session := mh.sessions[sessionName]
-    fmt.Println (session.payload)
 
     session.lock.RLock()
     w.Write([]byte(session.payload))
@@ -55,7 +85,6 @@ func (mh MainHandler) AddSession (session string) {
   if ok {
     return
   }
-
   sessions[session] = new(Session)
 }
 
@@ -68,11 +97,10 @@ func (mh MainHandler) StartSessions () {
 
     for {
       for sessionName, session := range mh.sessions {
-        rep, err := (*mh.rconn).Do ("GET", "sessions:" + sessionName + ":state")
-        payload , err := redis.String(rep, err)
-
+        payload , err := mh.store.GetStateForSession (sessionName)
         if err != nil {
           delete (mh.sessions, sessionName)
+          return
         }
 
         session.lock.Lock()
@@ -87,12 +115,14 @@ func (mh MainHandler) StartSessions () {
 func main () {
   runtime.GOMAXPROCS(runtime.NumCPU())
 
-  c, err := redis.Dial("tcp", ":6379")
+  rds := RedisDataStore{}
+  err := rds.Connect (":6379")
   if err != nil {
-    return;
+    return
   }
 
-  mh := MainHandler{make(map[string]*Session), &c}
+  mh := MainHandler{make(map[string]*Session), &rds}
+
   mh.AddSession("live")
   mh.StartSessions()
 

2271c89db8e9e87fc3870cde34ed5e2e469d4a63
Alberto Ruiz <aruiz@gnome.org>
2014-01-22 22:14:52 +0100

    Adding locks to the RedisDataStore and to the Cache methods

--- cache.go
+++ cache.go
@@ -2,7 +2,7 @@ package main
 
 import (
 //  "time"
-//  "sync"
+  "sync"
   "github.com/garyburd/redigo/redis"
 )
 
@@ -11,18 +11,9 @@ type DataStore interface {
   GetStateForSession (session string) (string, error)
 }
 
-func (rds RedisDataStore) GetStateForSession (session string) (string, error) {
-  if rds.conn == nil {
-    return "", nil
-  }
-
-  rep, err := (*rds.conn).Do ("GET", "sessions:" + session + ":state")
-  payload , err := redis.String(rep, err)
-
-  return payload, err
-}
 
 type RedisDataStore struct {
+  mut         *sync.Mutex
   conn        *redis.Conn
 }
 
@@ -32,13 +23,29 @@ func (rds RedisDataStore) Connect (url string) error {
     return err
   }
 
+  rds.mut = new(sync.Mutex)
+
   rds.conn = &c
   return nil
+}
+
+func (rds RedisDataStore) GetStateForSession (session string) (string, error) {
+  if rds.conn == nil {
+    return "", nil
+  }
+
+  rds.mut.Lock()
+  rep, err := (*rds.conn).Do ("GET", "sessions:" + session + ":state")
+  rds.mut.Unlock()
+
+  payload , err := redis.String(rep, err)
+  return payload, err
 }
 
 type Cache struct {
   ds DataStore
   cache map[string]string
+  lock *sync.RWMutex
 }
 
 func CreateCache () (Cache, error) {
@@ -50,9 +57,12 @@ func CreateCache () (Cache, error) {
   ds := new(RedisDataStore)
   ds.conn = &conn
 
-  go func (
+  lock := new(sync.RWMutex)
 
-  return Cache{ds}, nil
+  //go func ()
+
+  cache := Cache{ds, make(map[string]string), lock}
+  return cache, nil
 }
 
 func (self *Cache) SetPayload (key string, payload string) error {
@@ -60,6 +70,21 @@ func (self *Cache) SetPayload (key string, payload string) error {
 }
 
 func (self *Cache) GetPayload (key string) (string, error) {
-  if 
-  return  self.ds.GetStateForSession(key)
+  self.lock.RLock()
+  value, ok := self.cache[key]
+  self.lock.RUnlock()
+  if !ok {
+    v, err := self.ds.GetStateForSession(key)
+    if err != nil {
+      return "", err
+    }
+
+    value = v
+
+    self.lock.Lock ()
+    self.cache[key] = value
+    self.lock.Unlock ()
+  }
+
+  return value, nil
 }
